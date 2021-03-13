46ac2f217c187c45a6d9531e4168d2d705596896
Huvalk <akiraq@yandex.ru>
2021-03-13 16:39:28 +0300

    feat(invite): update team

--- application/invite/repository.go
+++ application/invite/repository.go
@@ -10,6 +10,7 @@ type Repository interface {
 	UpdateUserJoinedTeam(userID1 int, userID2 int, teamID int, eventID int) error
 	UpdateUserLeftTeam(userID int, teamID int, eventID int) error
 	UpdateUserChangedTeam(userID int, teamID int, eventID int) error
+	UpdateTeamMerged(teamFromID1 int, teamFromID2 int, teamToID int, eventID int)
 	UserInviteUser(invitation *models.Invitation) error
 	TeamInviteUser(invitation *models.Invitation) error
 	UserInviteTeam(invitation *models.Invitation) error
