155b77f3ac9915921ce7985b19168bb165517192
Huvalk <akiraq@yandex.ru>
2021-03-20 00:56:29 +0300

    fix: docker-compose ports

--- pkg/infrastructure/database.go
+++ pkg/infrastructure/database.go
@@ -7,7 +7,7 @@ import (
 	"os"
 )
 
-const dsn = `pool_max_conns=30 host=localhost port=1111 user=%s password=%s dbname=%s sslmode=disable`
+const dsn = `pool_max_conns=30 host=localhost port=8081 user=%s password=%s dbname=%s sslmode=disable`
 
 func InitDatabase() (*pgxpool.Pool, error) {
 	dsnFmt := fmt.Sprintf(dsn, os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
