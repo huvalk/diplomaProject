48bea950fc55ff25d8647fef4961d6cb4e36593c
Oleg <elizarov1999@list.ru>
2021-03-21 15:46:10 +0300

    fix: Add get event users method

--- application/event/usecase.go
+++ application/event/usecase.go
@@ -4,5 +4,6 @@ import "diplomaProject/application/models"
 
 type UseCase interface {
 	Get(id int) (*models.Event, error)
+	GetEventUsers(evtID int) (*models.UserArr, error)
 	Create(newEvent *models.Event) (*models.Event, error)
 }
