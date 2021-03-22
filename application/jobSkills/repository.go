3e011ff555a3dc8eaf249135fa06d2464283cced
Oleg <elizarov1999@list.ru>
2021-03-22 18:59:56 +0300

    new: addJob method

--- application/jobSkills/repository.go
+++ application/jobSkills/repository.go
@@ -5,8 +5,14 @@ import "diplomaProject/application/models"
 type Repository interface {
 	AddJob(uid int, newJob *models.Job) error
 	RemoveJob(uid, jid int) error
-	AddSkill(uid int, newSkill *models.Skills) error
-	RemoveSkill(uid, skid int) error
+	AddSkill(uid int, job *models.Job, newSkill *models.Skills) error
+	AddManySkills(uid, jbID int, skillsID []int) error
+	RemoveSkill(uid, jbID, skID int) error
+	RemoveAllSkills(uid, jbID int) error
 	GetAllJobs() (*[]models.Job, error)
 	GetSkillsByJob(jobName string) (*[]models.Skills, error)
+	GetJobByName(jobName string) (*models.Job, error)
+	GetJobByID(jobID int) (*models.Job, error)
+	CreateJob(jobName string) (*models.Job, error)
+	CreateSkill(skillName string, jbID int) (*models.Skills, error)
 }
--- application/models/skills.go
+++ application/models/skills.go
@@ -8,3 +8,17 @@ type Skills struct {
 
 //easyjson:json
 type SkillsArr []Skills
+
+type AddSkill struct {
+	JobName   string `json:"jobName"`
+	SkillName string `json:"skillName"`
+}
+
+type AddSkillID struct {
+	JobID     int    `json:"jobID"`
+	SkillName string `json:"skillName"`
+	SkillID   int    `json:"skillID"`
+}
+
+//easyjson:json
+type AddSkillIDArr []AddSkillID
