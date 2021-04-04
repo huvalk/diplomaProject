14056ed0e61bc880addeaa17dbe6b2611df42088
Huvalk <akiraq@yandex.ru>
2021-04-05 01:43:50 +0300

    trying deploy on prod

--- pkg/infrastructure/database.go
+++ pkg/infrastructure/database.go
@@ -7,7 +7,7 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-const dsn = `pool_max_conns=30 host=localhost port=8081 user=%s password=%s dbname=%s sslmode=disable`
+const dsn = `pool_max_conns=30 host=db port=5432 user=%s password=%s dbname=%s sslmode=disable`
 
 func InitDatabase() (*pgxpool.Pool, error) {
 	dsnFmt := fmt.Sprintf(dsn, globalVars.POSTGRES_USER, globalVars.POSTGRES_PASSWORD, globalVars.POSTGRES_DB)
