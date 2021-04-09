c4bfaf4fc4f6d8b93e334b975fc9fb46a2039bbb
Huvalk <akiraq@yandex.ru>
2021-04-09 12:13:01 +0300

    feat(Config): db host and nginx config

--- pkg/infrastructure/database.go
+++ pkg/infrastructure/database.go
@@ -7,10 +7,11 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-const dsn = `pool_max_conns=30 host=db port=5432 user=%s password=%s dbname=%s sslmode=disable`
+const dsn = `pool_max_conns=30 host=%s port=%s user=%s password=%s dbname=%s sslmode=disable`
 
 func InitDatabase() (*pgxpool.Pool, error) {
-	dsnFmt := fmt.Sprintf(dsn, globalVars.POSTGRES_USER, globalVars.POSTGRES_PASSWORD, globalVars.POSTGRES_DB)
+	dsnFmt := fmt.Sprintf(dsn, globalVars.POSTGRES_HOST, globalVars.POSTGRES_PORT,
+		globalVars.POSTGRES_USER, globalVars.POSTGRES_PASSWORD, globalVars.POSTGRES_DB)
 
 	config, err := pgxpool.ParseConfig(dsnFmt)
 	if err != nil {
