164511bb4414827263524ecbe8b77128f026479f
Huvalk <akiraq@yandex.ru>
2021-04-11 22:31:35 +0300

    fix(Invite): no invite ti accept

--- application/invite/repository/delete.go
+++ application/invite/repository/delete.go
@@ -124,12 +124,9 @@ func (r *InviteRepository) AcceptInvite(userID1 int, userID2 int, eventID int) e
 				and rejected = false
 				and approved = false`
 
-	res, err := r.conn.Exec(context.Background(), query, userID1, eventID, userID2)
+	_, err := r.conn.Exec(context.Background(), query, userID1, eventID, userID2)
 	if err != nil {
 		return err
-	}
-	if res.RowsAffected() == 0 {
-		return errors.New("no invite to accept")
 	}
 
 	return nil
